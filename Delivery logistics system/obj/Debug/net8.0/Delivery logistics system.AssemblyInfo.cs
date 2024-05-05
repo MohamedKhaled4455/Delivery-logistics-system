2a9a5205ace6baceca13e26b84647b59a4a1cd7b
Mohamed Khaled <88774059+MohamedKhaled4455@users.noreply.github.com>
2024-05-05 23:01:44 +0300

    11:01

--- Delivery logistics system/obj/Debug/net8.0/Delivery logistics system.AssemblyInfo.cs
+++ Delivery logistics system/obj/Debug/net8.0/Delivery logistics system.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Delivery logistics system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+124a347f898351bc57aa9e7228ccd5399f5e4c5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28fcda4da1df003cb7b57bf3cd6a334afa17abb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Delivery logistics system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Delivery logistics system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
