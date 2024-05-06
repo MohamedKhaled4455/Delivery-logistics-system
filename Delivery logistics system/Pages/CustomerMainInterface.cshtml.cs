022c968600c25ea8cd9c78e6aada380d0d3ba038
Mohamed Khaled <88774059+MohamedKhaled4455@users.noreply.github.com>
2024-05-06 19:35:11 +0300

    to be edited

---
+++ Delivery logistics system/Pages/CustomerMainInterface.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Delivery_logistics_system.Pages.Customer
+{
+    public class MainInterfaceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- Delivery logistics system/obj/Debug/net8.0/Delivery logistics system.AssemblyInfo.cs
+++ Delivery logistics system/obj/Debug/net8.0/Delivery logistics system.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Delivery logistics system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbcb2b85ae6b9e7efb7576a8d8c0f5b57ad5ad2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf1f0c6e50eaab3b59fdd391625bed78d2d4152e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Delivery logistics system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Delivery logistics system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
